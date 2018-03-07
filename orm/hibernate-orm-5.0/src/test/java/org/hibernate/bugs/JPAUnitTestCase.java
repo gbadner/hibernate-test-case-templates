d806d4ef5cf35da85efc51ce70c5e0648ce89006
Gail Badner <gbadner@redhat.com>
2018-03-07 14:16:50 -0800

    test case with unidirectional one-to-on referencing non-primary key

--- orm/hibernate-orm-5.0/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-5.0/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,12 +1,23 @@
 package org.hibernate.bugs;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
 import javax.persistence.Persistence;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -29,10 +40,43 @@ public class JPAUnitTestCase {
 	// Add your tests, using standard JUnit.
 	@Test
 	public void hhh123Test() throws Exception {
+		Product product = new Product();
+		ProductInfo productInfo = new ProductInfo();
+		product.id = 1;
+		productInfo.id = 2;
+		product.productInfo = productInfo;
+		productInfo.productId = product.id;
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
+		entityManager.persist( product );
+		entityManager.flush();
+		entityManager.clear();
+		product = entityManager.find( Product.class, product.id );
+		assertNotNull( product.productInfo );
+		assertEquals( 2, product.productInfo.id );
+		assertEquals( 1, product.productInfo.productId );
 		entityManager.getTransaction().commit();
 		entityManager.close();
+	}
+
+	@Entity(name = "Product")
+	public static class Product {
+		@Id
+		@Column(name = "id")
+		private int id;
+
+		@OneToOne(cascade = CascadeType.ALL)
+		@JoinColumn(name = "id", referencedColumnName = "productId", insertable = false, updatable = false)
+		private ProductInfo productInfo;
+
+	}
+
+	@Entity(name = "ProductInfo")
+	public static class ProductInfo{
+		@Id
+		private int id;
+
+		@Column(name = "productId", unique = true, updatable = false)
+		private int productId;
 	}
 }
